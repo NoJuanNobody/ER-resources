c67b3abab51fbf17cddc065abe8e90b24a3a29bf
alejandro londono <alondono11@gmail.com>
2015-12-18 16:43:46 -0500

    adding more versioning options to autocomplete.js

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -4,15 +4,19 @@ $(document).ready(function () {
 
   // phone
   var APL_callsource = "1-800-CALLSOURCE";
+  var CCI_callsource = "1-800-CALLSOURCE";
   var CCV_callsource = "1-800-CALLSOURCE";
   var CWF_callsource = "1-800-CALLSOURCE";
   var ETH_callsource = "1-800-CALLSOURCE";
+  var FRV_callsource = "1-800-CALLSOURCE";
   var GSV_callsource = "1-800-CALLSOURCE";
   var HSD_callsource = "1-800-CALLSOURCE";
+  var LPH_callsource = "1-800-CALLSOURCE";
   var OCV_callsource = "1-800-CALLSOURCE";
   var RWV_callsource = "1-800-CALLSOURCE";
   var TCK_callsource = "1-800-CALLSOURCE";
   var WCD_callsource = "1-800-CALLSOURCE";
+  var SBV_callsource = "1-800-CALLSOURCE";
   // FACEBOOK CALLSOURCE
   var FB_HSD_callsource = "1-800-CALLSOURCE";
   // EMAIL CALLSOURCE NUMBERS
@@ -25,6 +29,8 @@ $(document).ready(function () {
   var RWV_EMAIL_callsource = "1-800-CALLSOURCE";
   var TCK_EMAIL_callsource = "1-800-CALLSOURCE";
   var WCD_EMAIL_callsource = "1-800-CALLSOURCE";
+
+  // include FRV CCI and LPH in email CS numbers
   // Level of Care
   var APL_care = "memory care or long-term care.";
   var CCV_care = "assisted living, memory care, or skilled nursing.";
@@ -173,7 +179,40 @@ $(document).ready(function () {
       $('.disclaimer').text(WCD_disclaimer);
       communityName = 'wind-crest';
     }
+  } else if (window.location.search.indexOf('from=FRV') > -1) {
+    if (window.location.search.indexOf('from=FRV-EMAIL') > -1) {
+      $(".phone").text(FRV_EMAIL_callsource);
+      communityName = 'fox-run';
+    } else{
+      $(".phone").text(FRV_callsource);
+      communityName = 'fox-run';
+    }
+  }  else if (window.location.search.indexOf('from=CCI') > -1) {
+    if (window.location.search.indexOf('from=CCI-EMAIL') > -1) {
+      $(".phone").text(CCI_EMAIL_callsource);
+      communityName = 'cedar-crest';
+    } else{
+      $(".phone").text(CCI_callsource);
+      communityName = 'cedar-crest';
+    }
+  }  else if (window.location.search.indexOf('from=LPH') > -1) {
+    if (window.location.search.indexOf('from=LPH-EMAIL') > -1) {
+      $(".phone").text(LPH_EMAIL_callsource);
+      communityName = 'linden-ponds';
+    } else{
+      $(".phone").text(LPH_callsource);
+      communityName = 'linden-ponds';
+    }
+  }else if (window.location.search.indexOf('from=SBV') > -1) {
+    if (window.location.search.indexOf('from=SBV-EMAIL') > -1) {
+      $(".phone").text(SBV_EMAIL_callsource);
+      communityName = 'seabrook';
+    } else{
+      $(".phone").text(SBV_callsource);
+      communityName = 'seabrook';
+    }
   }
+// form options
 
   if (communityName === 'ashby-ponds') {
     var newOptionMC = $('<option value="MC">Memory Care</option>');
@@ -208,8 +247,8 @@ $(document).ready(function () {
   }
   
   else if (communityName === 'greenspring') {
-	var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
-	$('#field18').append(newOptionSNF);
+  var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+  $('#field18').append(newOptionSNF);
   }
   
   else if (communityName === 'highland-springs') {
@@ -218,9 +257,9 @@ $(document).ready(function () {
   
   else if (communityName === 'oak-crest') {
     var newOptionALF = $('<option value="ALF">Assisted Living</option>');
-	var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
- 	$('#field18').append(newOptionALF);
-	$('#field18').append(newOptionSNF);
+  var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+  $('#field18').append(newOptionALF);
+  $('#field18').append(newOptionSNF);
   }
   
   else if (communityName === 'riderwood') {
@@ -241,6 +280,43 @@ $(document).ready(function () {
     var newOptionMC = $('<option value="MC">Memory Care</option>');
     var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
     $('#field18').append(newOptionMC);
+    $('#field18').append(newOptionSNF);
+  }
+  else if (communityName === 'fox-run') {
+    var newOptionMC = $('<option value="MC">Assisted living</option>');
+    var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+
+    $('#field18').append(newOptionMC);
+    $('#field18').append(newOptionSNF);
+
+  }
+  else if (communityName === 'cedar-crest') {
+    var newOptionMC = $('<option value="MC">Assisted living</option>');
+    var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+
+    $('#field18').append(newOptionMC);
+    $('#field18').append(newOptionSNF);
+
+  }
+  else if (communityName === 'cedar-crest') {
+    var newOptionALF = $('<option value="MC">Assisted living</option>');
+    var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+    var newOptionMC = $('<option value="MC">Memory Care</option>');
+    $('#field18').append(newOptionMC);
+    $('#field18').append(newOptionSNF);
+    $('#field18').append(newOptionALF);
+
+  }  else if (communityName === 'seabrook') {
+    var newOptionALF = $('<option value="MC">Assisted living</option>');
+    var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
+    var newOptionMC = $('<option value="MC">Memory Care</option>');
+    $('#field18').append(newOptionMC);
+    $('#field18').append(newOptionSNF);
+    $('#field18').append(newOptionALF);
+
+  } 
+  else if (communityName === 'linden-ponds') {
+    var newOptionSNF = $('<option value="SNF">Long Term Care</option>');
     $('#field18').append(newOptionSNF);
   }
   
